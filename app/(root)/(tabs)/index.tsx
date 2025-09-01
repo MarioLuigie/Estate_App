b6dc06f53b029a47514559715ac67a7ecec34ccd
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-09-01 23:02:50 +0200

    Fix: Fix the globals.css path in metro.config.js

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'expo-router';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 export default function Index() {
 	return (
 		<View style={styles.container}>
-			<Link href='/sign-in'>Sign In</Link>
+			<Text className='text-6xl text-orange-500'>WELCOME!!!</Text>
+			<Link href='/sign-in'>Sign</Link>
 			<Link href='/explore'>Explore</Link>
 			<Link href='/profile'>Profile</Link>
 			<Link href='/properties/1'>Properties</Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{ts,tsx}",          // wszystkie pliki w app/ (layouty, indexy, tabs, auth, properties)
+    "./components/**/*.{ts,tsx}",   // wszystkie komponenty
+  ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
