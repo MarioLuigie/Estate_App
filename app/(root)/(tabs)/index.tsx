15edecd9ac45ae037b46dd179cc74745b1f16e49
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-09-01 22:11:21 +0200

    Docs: Install NativeWind and init tailwindcss

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+import { View, StyleSheet } from 'react-native';
 
 export default function Index() {
 	return (
 		<View style={styles.container}>
-			<Text>React Native first app.</Text>
+			<Link href='/sign-in'>Sign In</Link>
+			<Link href='/explore'>Explore</Link>
+			<Link href='/profile'>Profile</Link>
+			<Link href='/properties/1'>Properties</Link>
 		</View>
 	);
 }
