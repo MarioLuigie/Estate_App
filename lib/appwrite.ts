afab87b7f509ea3351f815a8773f5eaad7777878
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-09-05 00:18:25 +0200

    Feat: Create getPropertyById in appwrite.ts

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -114,7 +114,6 @@ export async function getCurrentUser() {
 				});
 
 				avatarBase64 = arrayBufferToBase64(buffer);
-
 			} catch (avatarError) {
 				console.warn('Failed to generate avatar initials:', avatarError);
 				avatarBase64 = undefined;
@@ -134,55 +133,70 @@ export async function getCurrentUser() {
 }
 
 export async function getLatestProperties() {
-  try {
-    const result = await databases.listDocuments(
-      config.databaseId!,
-      config.propertiesCollectionId!,
-      [Query.orderAsc("$createdAt"), Query.limit(5)]
-    );
+	try {
+		const result = await databases.listDocuments(
+			config.databaseId!,
+			config.propertiesCollectionId!,
+			[Query.orderAsc('$createdAt'), Query.limit(5)]
+		);
 
-    return result.documents;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
+		return result.documents;
+	} catch (error) {
+		console.error(error);
+		return [];
+	}
 }
 
 export async function getProperties({
-  filter,
-  query,
-  limit,
+	filter,
+	query,
+	limit,
 }: {
-  filter: string;
-  query: string;
-  limit?: number;
+	filter: string;
+	query: string;
+	limit?: number;
 }) {
-  try {
-    const buildQuery = [Query.orderDesc("$createdAt")];
+	try {
+		const buildQuery = [Query.orderDesc('$createdAt')];
 
-    if (filter && filter !== "All")
-      buildQuery.push(Query.equal("type", filter));
+		if (filter && filter !== 'All')
+			buildQuery.push(Query.equal('type', filter));
 
-    if (query)
-      buildQuery.push(
-        Query.or([
-          Query.search("name", query),
-          Query.search("address", query),
-          Query.search("type", query),
-        ])
-      );
+		if (query)
+			buildQuery.push(
+				Query.or([
+					Query.search('name', query),
+					Query.search('address', query),
+					Query.search('type', query),
+				])
+			);
 
-    if (limit) buildQuery.push(Query.limit(limit));
+		if (limit) buildQuery.push(Query.limit(limit));
 
-    const result = await databases.listDocuments(
-      config.databaseId!,
-      config.propertiesCollectionId!,
-      buildQuery
-    );
+		const result = await databases.listDocuments(
+			config.databaseId!,
+			config.propertiesCollectionId!,
+			buildQuery
+		);
 
-    return result.documents;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
+		return result.documents;
+	} catch (error) {
+		console.error(error);
+		return [];
+	}
+}
+
+export async function getPropertyById({ id }: { id: string }) {
+	try {
+		const result = databases.getDocument(
+			config.databaseId!,
+			config.propertiesCollectionId!,
+			id
+		);
+
+		return result;
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
 }
