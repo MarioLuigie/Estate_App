721b7c487c846643ced9fd8e27f5a6327cf15110
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-09-09 01:59:11 +0200

    Feat: Add createProperty async func into appwrite.ts

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -7,25 +7,11 @@ import {
 	Avatars,
 	Client,
 	Databases,
+	ID,
 	OAuthProvider,
 	Query,
 	Storage,
 } from 'react-native-appwrite';
-
-// export const config = {
-// 	platform: 'com.mlotocki.estate',
-// 	endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-// 	projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-// 	databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
-// 	galleriesCollectionId:
-// 		process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
-// 	reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
-// 	agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
-// 	propertiesCollectionId:
-// 		process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
-// 	bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID,
-// 	googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
-// };
 
 export const config = {
 	platform: 'com.mlotocki.estate',
@@ -175,7 +161,7 @@ export async function getLatestProperties(limit: number | string = 5) {
 		const result = await databases.listDocuments(
 			config.databaseId!,
 			config.propertiesCollectionId!,
-			[Query.orderAsc('$createdAt'), Query.limit(Number(parsedLimit))]
+			[Query.orderDesc('$createdAt'), Query.limit(Number(parsedLimit))]
 		);
 
 		return result.documents;
@@ -327,3 +313,23 @@ export async function getMyProperties({ userId }: { userId: string }) {
 //   );
 //   return result.documents;
 // }
+
+export async function createProperty(property: any) {
+	try {
+		const result = await databases.createDocument(
+			config.databaseId!,
+			config.propertiesCollectionId!,
+			ID.unique(),
+			{
+				...property,
+				geolocation: `${property.latitude},${property.longitude}`,
+				gallery: property.image || [],
+				reviews: [],
+			}
+		);
+
+		return result;
+	} catch (error) {
+		console.error("Property not created", error)
+	}
+}
