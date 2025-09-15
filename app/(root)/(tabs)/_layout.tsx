66654ac158bf6e02b03683378f70c8f3015ea032
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-09-15 20:44:51 +0200

    Feat: Add absolute icon button for fast creating property in /tabs/_layout.tsx

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,10 +1,11 @@
-// modules
-import { Tabs } from 'expo-router';
-import { Image, ImageSourcePropType, Text, View } from 'react-native';
+import { Tabs, useRouter } from 'expo-router';
+import { Image, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-// lib
 import icons from '@/lib/constants/icons';
 import { TABS_HEIGHT } from '@/lib/constants/layout';
+import { ROUTES } from '@/lib/constants/paths';
+import IconButton from '@/components/ui/IconButton';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const TabIcon = ({
 	focused,
@@ -12,7 +13,7 @@ const TabIcon = ({
 	title,
 }: {
 	focused: boolean;
-	icon: ImageSourcePropType;
+	icon: any;
 	title: string;
 }) => (
 	<View className="flex-1 mt-4 flex flex-col items-center">
@@ -35,60 +36,94 @@ const TabIcon = ({
 );
 
 export default function TabsLayout() {
-	const insets = useSafeAreaInsets()
+	const insets = useSafeAreaInsets();
+	const router = useRouter();
+
 	return (
-		<Tabs
-			screenOptions={{
-				tabBarShowLabel: false,
-				tabBarStyle: {
-					backgroundColor: 'white',
-					opacity: 0.9,
+		<View style={{ flex: 1 }}>
+			{/* Tabs */}
+			<Tabs
+				screenOptions={{
+					tabBarShowLabel: false,
+					tabBarStyle: {
+						backgroundColor: 'white',
+						opacity: 0.9,
+						position: 'absolute',
+						borderTopColor: '#0061FF1A',
+						borderTopWidth: 1,
+						height: TABS_HEIGHT,
+						bottom: insets.bottom,
+					},
+				}}
+			>
+				<Tabs.Screen
+					name="index"
+					options={{
+						title: 'Home',
+						headerShown: false,
+						tabBarIcon: ({ focused }) => (
+							<TabIcon
+								focused={focused}
+								icon={icons.home}
+								title="Home"
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="explore"
+					options={{
+						title: 'Explore',
+						headerShown: false,
+						tabBarIcon: ({ focused }) => (
+							<TabIcon
+								focused={focused}
+								icon={icons.search}
+								title="Explore"
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="profile"
+					options={{
+						title: 'Profile',
+						headerShown: false,
+						tabBarIcon: ({ focused }) => (
+							<TabIcon
+								focused={focused}
+								icon={icons.person}
+								title="Profile"
+							/>
+						),
+					}}
+				/>
+			</Tabs>
+
+			{/* FAB */}
+			<IconButton
+				icon={<MaterialIcons name="add" size={24} color="white" />}
+				onPress={() =>
+					router.push({ pathname: ROUTES.PROPERTIES_ADD_PROPERTY })
+				}
+				containerStyle={{
 					position: 'absolute',
-					borderTopColor: '#0061FF1A',
-					borderTopWidth: 1,
-					height: TABS_HEIGHT,
-					bottom: insets.bottom
-				},
-			}}
-		>
-			<Tabs.Screen
-				name="index"
-				options={{
-					title: 'Home',
-					headerShown: false,
-					tabBarIcon: ({ focused }) => (
-						<TabIcon focused={focused} icon={icons.home} title="Home" />
-					),
+					bottom: TABS_HEIGHT + 60, // trochę nad tabbarem
+					right: 20,
+					alignSelf: 'center',
+					backgroundColor: 'rgb(0, 97, 255)',
+					borderRadius: 40,
+					width: 64,
+					height: 64,
+					justifyContent: 'center',
+					alignItems: 'center',
+					elevation: 5, // cień na Androidzie
+					shadowColor: '#000', // cień na iOS
+					shadowOpacity: 0.2,
+					shadowOffset: { width: 0, height: 2 },
+					shadowRadius: 4,
 				}}
 			/>
-			<Tabs.Screen
-				name="explore"
-				options={{
-					title: 'Explore',
-					headerShown: false,
-					tabBarIcon: ({ focused }) => (
-						<TabIcon
-							focused={focused}
-							icon={icons.search}
-							title="Explore"
-						/>
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="profile"
-				options={{
-					title: 'Profile',
-					headerShown: false,
-					tabBarIcon: ({ focused }) => (
-						<TabIcon
-							focused={focused}
-							icon={icons.person}
-							title="Profile"
-						/>
-					),
-				}}
-			/>
-		</Tabs>
+		</View>
 	);
 }
