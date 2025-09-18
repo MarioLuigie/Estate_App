dd4cdf7a3591015b6b59f72ce2888797ed28f9cf
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-09-18 21:02:23 +0200

    Refactor: Add useSafeInsets for bottom padding in add-property component

--- app/(root)/properties/add/add-property.tsx
+++ app/(root)/properties/add/add-property.tsx
@@ -1,16 +1,18 @@
 // modules
 import React from 'react';
 import { ScrollView } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 // lib
 import { ActionTypes } from '@/lib/constants/enums';
 // components
 import PropertyForm from '@/components/forms/PropertyForm';
 
 export default function AddProperty() {
+	const insets = useSafeAreaInsets();
 	return (
 		<ScrollView
 			showsVerticalScrollIndicator={false}
-			contentContainerStyle={{ paddingBottom: 48 }}
+			contentContainerStyle={{ paddingBottom: insets.bottom }}
 			className="flex-1 px-5"
 		>
       <PropertyForm actionType={ActionTypes.CREATE}/>
