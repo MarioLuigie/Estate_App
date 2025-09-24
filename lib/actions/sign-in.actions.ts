9c9185602a68b6524abad12f39f232d43c03a892
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-09-24 17:44:27 +0200

    Feat: Add phone key for user object in database in login function in sign-in.actions.ts

--- lib/actions/sign-in.actions.ts
+++ lib/actions/sign-in.actions.ts
@@ -86,6 +86,7 @@ export async function login() {
 					fullName: currentAuthUser.name,
 					email: currentAuthUser.email,
 					createdAt: currentAuthUser.$createdAt,
+					phone: null,
 				}
 			);
 		}
