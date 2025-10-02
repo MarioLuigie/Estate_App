40fb1dced9e71aee9c3be867d3781bcc44e1a507
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-10-02 22:06:43 +0200

    Feat: Create new build for compiling server functions to js

--- functions/dist/paypal/create-order/index.js
+++ functions/dist/paypal/create-order/index.js
@@ -3,14 +3,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = createOrder;
 const paypalAuth_1 = require("../utils/paypalAuth");
 const index_1 = require("../../appwrite-client/index");
+const node_appwrite_1 = require("node-appwrite");
 async function createOrder(event, context) {
     try {
-        // auth user
-        const authUser = await index_1.account.get();
         const rawInput = event.req.body || '{}';
         const parsed = JSON.parse(rawInput);
+        // auth user
+        // wyciągam sam token string
+        const jwtString = parsed.jwt.jwt; // zamiast JSON.parse(event.req.body.jwt)
+        // buduje klienta z JWT
+        const client = new node_appwrite_1.Client()
+            .setEndpoint(process.env.APPWRITE_ENDPOINT)
+            .setProject(process.env.APPWRITE_PROJECT_ID)
+            .setJWT(jwtString);
+        const account = new node_appwrite_1.Account(client);
+        const authUser = await account.get();
         const { property, totalPrice, startDate, endDate, paymentMethod, fullName, email, phone, currency = 'USD', returnUrl, cancelUrl, } = parsed;
-        // logujemy co naprawdę mamy
+        // loguje co naprawdę mamy
         console.log('Parsed input:', parsed);
         // const { amount, currency, bookingId, returnUrl, cancelUrl } =
         // 	parsed.data as CreateOrderInput;
@@ -19,7 +28,7 @@ async function createOrder(event, context) {
         // 	throw new Error(`Invalid amount: ${amount}`);
         // if (!currency) throw new Error(`Invalid currency: ${currency}`);
         // if (!bookingId) throw new Error(`Missing bookingId`);
-        // tworzymy dokument booking w DB
+        // tworze dokument booking w DB
         const bookingDoc = await index_1.databases.createDocument(process.env.APPWRITE_DATABASE_ID, process.env.APPWRITE_BOOKINGS_COLLECTION_ID, index_1.ID.unique(), {
             ownerId: authUser.$id,
             property,
@@ -35,16 +44,19 @@ async function createOrder(event, context) {
             createdAt: new Date().toISOString(),
         });
         const bookingId = bookingDoc.$id;
-        // pobieramy token PayPal
+        // pobieram token PayPal
         const accessToken = await (0, paypalAuth_1.getAccessToken)();
         if (!accessToken)
             throw new Error('No PayPal access token');
-        // budujemy zamówienie
+        // buduje zamówienie
         const body = {
             intent: 'CAPTURE',
             purchase_units: [
                 {
-                    amount: { currency_code: currency, value: totalPrice.toFixed(2) },
+                    amount: {
+                        currency_code: currency,
+                        value: totalPrice.toFixed(2),
+                    },
                     custom_id: bookingId,
                 },
             ],
@@ -53,7 +65,7 @@ async function createOrder(event, context) {
                 cancel_url: cancelUrl ?? process.env.PAYPAL_CANCEL_URL,
             },
         };
-        // wywołanie PayPal API
+        // wywołołuje PayPal API
         const response = await fetch('https://api-m.sandbox.paypal.com/v2/checkout/orders', {
             method: 'POST',
             headers: {
